46cb964bd7939d0ecd21c228ee0b170647492505
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-11-19 23:07:55 -0300

    continuar

--- demo/src/main/java/br/edu/infuse/app/vo/EntityVo.java
+++ demo/src/main/java/br/edu/infuse/app/vo/EntityVo.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+//Value Object (VO) Patterns
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
